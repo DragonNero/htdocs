c1b794b5c78196938297a082a77ff03bfaaf9437
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-17 21:35:19 +0200

    added array functions

--- index.php
+++ index.php
@@ -4,11 +4,20 @@
 <html>
 <body>
 
+  <?php
+  $search_array = array('first' => 1, 'second' => 4);
+  if (array_key_exists('first', $search_array)) {
+      echo "The 'first' element is in the array";
+  }
+  ?>
+
 <form action="form_process.php" method="post">
 Name: <input type="text" name="name" placeholder="Enter username"><br>
 E-mail: <input type="text" name="email" placeholder="Enter email"><br>
 <input type="submit" name="submit">
 </form>
+
+
 
 </body>
 </html>
