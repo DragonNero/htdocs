bcf465e80ffc5e0d62350f31cff47c474d8c730a
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-18 22:05:34 +0200

    added login form

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="col-s-6">
+      <form class="" action="login.php" method="post">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" name="username" value="" class="form-control">
+
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="text" name="password" value="" class="password">
+
+        </div>
+
+        <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+
+      </form>
+
+    </div>
+
+  </div>
+
+  <?php
+
+
+  ?>
+
+
+</body>
+</html>
