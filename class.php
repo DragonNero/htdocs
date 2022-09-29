ad8790e1c764c5f724e3357ad96517460d83f688
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-29 22:47:26 +0200

    added class and verified that it is exists

---
+++ class.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+<?php
+class Car {
+}
+
+if(class_exists("Car")) {
+  echo "Yes";
+} else {
+  echo "No";
+}
+ ?>
