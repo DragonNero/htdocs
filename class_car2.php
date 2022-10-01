12265761706fccb2eff91884ee9ff3f936071175
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-10-01 18:23:36 +0200

    called a method of my object bmw

---
+++ class_car2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+<?php
+class Car {
+
+  function MoveWheels() {
+    echo "Moving wheels";
+
+  }
+}
+
+$bmw = new Car();
+
+
+$bmw->MoveWheels();
+
+
+ ?>
