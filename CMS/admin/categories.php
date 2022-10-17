110168c8904d52caae7b838957b6fbcc6a3fbc67
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-10-17 23:19:55 +0200

    deleted second edit form

--- CMS/admin/categories.php
+++ CMS/admin/categories.php
@@ -51,7 +51,6 @@
                             </div>
 
                           </form>
-                          <?php include "includes/edit_categories.php";?>
 
                           <?php
 
