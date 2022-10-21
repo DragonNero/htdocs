f0773240cbef86e11fa4439da17cdfdd039f0089
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-10-21 23:07:04 +0200

    added post page in admin

---
+++ CMS/admin/posts.php
@@ -0,0 +1,53 @@
+<?php include "includes/admin_header.php"; ?>
+
+<div id="wrapper">
+  <div id="page-wrapper">
+
+    <?php include "includes/admin_navigation.php"; ?>
+
+    <div class="container-fluid">
+
+      <!-- Page Heading -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">
+              Welcome to admin
+              <small>Author</small>
+          </h1>
+          <table class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Post Id</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Status</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Comments</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>kn</td>
+                <td>;kn</td>
+                <td>jh</td>
+                <td>dfx</td>
+                <td>utyf</td>
+                <td>;lg</td>
+                <td>dx</td>
+                <td>jgvg</td>
+                <td>oij</td>
+              </tr>
+            </tbody>
+          </table>
+
+
+        </div>
+
+      </div>   <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+</div>
+<?php include "includes/admin_footer.php";?>
