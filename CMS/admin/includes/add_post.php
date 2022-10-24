757720b4b7abe6326a2477dd3a4f0c486feff758
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-10-24 22:33:06 +0200

    added form

--- CMS/admin/includes/add_post.php
+++ CMS/admin/includes/add_post.php
@@ -1,41 +1,44 @@
 <?php
-
+  include "../../includes/db.php";
+  include "../admin_functions.php";
 
    if(isset($_POST['create_post'])) {
 
-            $post_title        = escape($_POST['title']);
-            $post_user         = escape($_POST['post_user']);
-            $post_category_id  = escape($_POST['post_category']);
-            $post_status       = escape($_POST['post_status']);
+            $post_title        = $_POST['title'];
+            $post_author       = $_POST['post_author'];
+            $post_category_id  = $_POST['post_category_id'];
+            $post_status       = $_POST['post_status'];
 
-            $post_image        = escape($_FILES['image']['name']);
-            $post_image_temp   = escape($_FILES['image']['tmp_name']);
-
-
-            $post_tags         = escape($_POST['post_tags']);
-            $post_content      = escape($_POST['post_content']);
-            $post_date         = escape(date('d-m-y'));
+            $post_image        = $_FILES['image']['name'];
+            $post_image_temp   = $_FILES['image']['tmp_name'];
 
 
-        move_uploaded_file($post_image_temp, "../images/$post_image" );
+            $post_tags         = $_POST['post_tags'];
+            $post_content      = $_POST['post_content'];
+            $post_date         = date('d-m-y');
+            $post_comment_count = 4;
+    }
 
 
-      $query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date,post_image,post_content,post_tags,post_status) ";
-
-      $query .= "VALUES({$post_category_id},'{$post_title}','{$post_user}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";
-
-      $create_post_query = mysqli_query($connection, $query);
-
-      confirmQuery($create_post_query);
-
-      $the_post_id = mysqli_insert_id($connection);
-
-
-      echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
-
-
-
-   }
+        // move_uploaded_file($post_image_temp, "../images/$post_image" );
+   //
+   //
+   //    $query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date,post_image,post_content,post_tags,post_status) ";
+   //
+   //    $query .= "VALUES({$post_category_id},'{$post_title}','{$post_user}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";
+   //
+   //    $create_post_query = mysqli_query($connection, $query);
+   //
+   //    // confirmQuery($create_post_query);
+   //
+   //    $the_post_id = mysqli_insert_id($connection);
+   //
+   //
+   //    echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
+   //
+   //
+   //
+   // }
 
 
 
@@ -54,25 +57,25 @@
        <label for="category">Category</label>
        <select name="post_category" id="">
 
-<?php
+<!-- //<?php
 
-        $query = "SELECT * FROM categories";
-        $select_categories = mysqli_query($connection,$query);
+        // $query = "SELECT * FROM categories";
+        // $select_categories = mysqli_query($connection,$query);
+        //
+        // //confirmQuery($select_categories);
+        //
+        //
+        // while($row = mysqli_fetch_assoc($select_categories )) {
+        // $cat_id = $row['cat_id'];
+        // $cat_title = $row['cat_title'];
+        //
+        //
+        //     echo "<option value='$cat_id'>{$cat_title}</option>";
+        //
+        //
+        // }
 
-        confirmQuery($select_categories);
-
-
-        while($row = mysqli_fetch_assoc($select_categories )) {
-        $cat_id = $row['cat_id'];
-        $cat_title = $row['cat_title'];
-
-
-            echo "<option value='$cat_id'>{$cat_title}</option>";
-
-
-        }
-
-?>
+// ?>-->
 
 
        </select>
@@ -86,21 +89,21 @@
 
 <?php
 
-        $users_query = "SELECT * FROM users";
-        $select_users = mysqli_query($connection,$users_query);
-
-        confirmQuery($select_users);
-
-
-        while($row = mysqli_fetch_assoc($select_users)) {
-        $user_id = $row['user_id'];
-        $username = $row['username'];
-
-
-            echo "<option value='{$username}'>{$username}</option>";
-
-
-        }
+        // $users_query = "SELECT * FROM users";
+        // $select_users = mysqli_query($connection,$users_query);
+        //
+        // // confirmQuery($select_users);
+        //
+        //
+        // while($row = mysqli_fetch_assoc($select_users)) {
+        // $user_id = $row['user_id'];
+        // $username = $row['username'];
+        //
+        //
+        //     echo "<option value='{$username}'>{$username}</option>";
+        //
+        //
+        // }
 
 ?>
 
@@ -113,10 +116,10 @@
 
 
 
-      <!-- <div class="form-group">
+      <div class="form-group">
          <label for="title">Post Author</label>
           <input type="text" class="form-control" name="author">
-      </div> -->
+      </div>
 
 
 
