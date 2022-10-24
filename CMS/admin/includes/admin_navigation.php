26ad6aaa8b49d63a2aa2a1bdccc736aeb9e6d764
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-10-24 21:56:18 +0200

    connected buttons in navigation to pages

--- CMS/admin/includes/admin_navigation.php
+++ CMS/admin/includes/admin_navigation.php
@@ -43,10 +43,10 @@
                 <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="posts.php">View all posts</a>
+                        <a href="includes/view_all_posts.php">View all posts</a>
                     </li>
                     <li>
-                        <a href="#">Add post</a>
+                        <a href="includes/add_post.php">Add post</a>
                     </li>
                 </ul>
             </li>
