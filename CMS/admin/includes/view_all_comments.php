515e4af851cd7fa8e0378f9072694dd15241d2cf
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-10-31 20:27:37 +0100

    added comments table

--- CMS/admin/includes/view_all_comments.php
+++ CMS/admin/includes/view_all_comments.php
@@ -1,61 +1,81 @@
 <table class="table table-bordered table-hover">
   <thead>
     <tr>
-      <th>Post Id</th>
+      <th>Id</th>
       <th>Author</th>
-      <th>Title</th>
-      <th>Category</th>
+      <th>Comment</th>
+      <th>Email</th>
       <th>Status</th>
-      <th>Image</th>
-      <th>Tags</th>
-      <th>Comments</th>
+      <th>In Response to</th>
       <th>Date</th>
+      <th>Approved</th>
+      <th>Unapproved</th>
+      <th>Delete</th>
     </tr>
   </thead>
   <tbody>
+
+
+
     <?php
-    $query = "SELECT * FROM posts";
-    $select_posts = mysqli_query($connection, $query);
 
-       while ($row = mysqli_fetch_assoc($select_posts)) {
-         $post_id = $row['post_id'];
-         $post_author = $row['post_author'];
-         $post_title = $row['post_title'];
-         $post_category_id = $row['post_category_id'];
-         $post_status = $row['post_status'];
-         $post_image = $row['post_image'];
-         $post_tags = $row['post_tags'];
-         $post_comment_count = $row['post_comment_count'];
-         $post_date = $row['post_date'];
+      $query = "SELECT * FROM comments";
 
-         echo "<tr>";
-         echo "<td>$post_id</td>";
-         echo "<td>$post_author</td>";
-         echo "<td>$post_title</td>";
+      $select_comments = mysqli_query($connection,$query);
+  //    $select_comments=  mysqli_query($connection,$query);
+
+      while($row = mysqli_fetch_assoc($select_comments)) {
+          $comment_id          = $row['comment_id'];
+          $comment_post_id     = $row['comment_post_id'];
+          $comment_author      = $row['comment_author'];
+          $comment_content     = $row['comment_content'];
+          $comment_email       = $row['comment_email'];
+          $comment_status      = $row['comment_status'];
+          $comment_date        = $row['comment_date'];
 
 
-         $query = "SELECT * FROM categories WHERE cat_id=$post_category_id ";
-         $select_categories_id = mysqli_query($connection, $query);
+          echo "<tr>";
 
-         while ($row = mysqli_fetch_assoc($select_categories_id)) {
-           $cat_id = $row['cat_id'];
-           $cat_title = $row['cat_title'];
+          ?>
 
-           echo "<td>$cat_title</td>";
-        }
+   <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $comment_id; ?>'></td>
 
 
+          <?php
+
+          echo "<td>$comment_id </td>";
+          echo "<td>$comment_author</td>";
+          echo "<td>$comment_content</td>";
+
+  //
+  //        $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
+  //        $select_categories_id = mysqli_query($connection,$query);
+  //
+  //        while($row = mysqli_fetch_assoc($select_categories_id)) {
+  //        $cat_id = $row['cat_id'];
+  //        $cat_title = $row['cat_title'];
+  //
+  //
+  //        echo "<td>{$cat_title}</td>";
+  //
+  //        }
+  //
+
+          echo "<td>$comment_email</td>";
+          echo "<td>$comment_status</td>";
 
 
-         echo "<td>$post_status</td>";
-         echo "<td><img width='100' src='../images/$post_image'</td>";
-         echo "<td>$post_tags</td>";
-         echo "<td>$post_comment_count</td>";
-         echo "<td>$post_date</td>";
-         echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'> Edit</a></td>";
-         echo "<td><a href='posts.php?delete={$post_id}'> Delete</a></td>";
-         echo "</tr>";
-    }
+          $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+          $select_post_id_query = mysqli_query($connection,$query);
+          while($row = mysqli_fetch_assoc($select_post_id_query)){
+          $post_id = $row['post_id'];
+          $post_title = $row['post_title'];
+
+              echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+
+
+          }}
+
     ?>
 
 
