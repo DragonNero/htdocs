698c179a382c2571962af1cb6538c5d522cbb5c7
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-11-07 11:52:44 +0100

    created login form

--- CMS/includes/sidebar.php
+++ CMS/includes/sidebar.php
@@ -3,19 +3,23 @@
 
 
 
-    <!-- Blog Search Well -->
+    <!-- Login -->
     <div class="well">
-        <h4>Blog Search</h4>
-        <form class="" action="index.php" method="post">
+        <h4>Login</h4>
+        <form class="" action="includes/login.php" method="post">
 
 
+        <div class="form-group">
+            <input name="user_name" type="text" class="form-control" placeholder="Enter Username">
+
+        </div>
         <div class="input-group">
-            <input name="search" type="text" class="form-control">
+            <input name="password" type="password" class="form-control" placeholder="Enter Password">
             <span class="input-group-btn">
-                <button name="submit" class="btn btn-default" type="submit">
-                    <span class="glyphicon glyphicon-search"></span>
-            </button>
+              <button class="btn btn-primary" type="submit" name="login">Submit</button>
+
             </span>
+
         </div>
       </form> <!-- search form -->
         <!-- /.input-group -->
